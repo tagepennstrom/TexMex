95d69ed1e6258465516566cadcc5bc1b7ff91dc4
Mauritz Sjödin <mauritz.sjodin3@gmail.com>
2025-04-04 21:37:31 +0200

    feat: implementera api endpoint som kompilerar latex till pdf
    close #3

--- server.go
+++ server.go
@@ -3,15 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"os/exec"
 
 	"github.com/coder/websocket"
 	"github.com/coder/websocket/wsjson"
 )
 
-const FRONTEND_HOST = "localhost:8000"
-const SERVER_ADDRESS = "localhost:8080"
+const frontendHost = "localhost:8000"
+const serverAddress = "localhost:8080"
 
 type EditDocMessage struct {
 	Document string `json:"document"`
@@ -30,7 +33,7 @@ func getDocument(w http.ResponseWriter, r *http.Request) {
 
 func editDocWebsocketHandler(w http.ResponseWriter, r *http.Request) {
 	opts := websocket.AcceptOptions{
-		OriginPatterns: []string{FRONTEND_HOST},
+		OriginPatterns: []string{frontendHost},
 	}
 	c, err := websocket.Accept(w, r, &opts)
 	if err != nil {
@@ -52,9 +55,44 @@ func editDocWebsocketHandler(w http.ResponseWriter, r *http.Request) {
 		document = editDocMessage.Document
 	}
 }
+func compileDocument(w http.ResponseWriter, r *http.Request) {
+	document, err := io.ReadAll(r.Body)
+	if err != nil {
+		errorMessage := fmt.Sprintf("Error reading request body: %s", err)
+		log.Println(errorMessage)
+		http.Error(w, errorMessage, http.StatusBadRequest)
+		return
+	}
+	r.Body.Close()
+
+	const filename = "document"
+	filenameLatex := fmt.Sprintf("%s.tex", filename)
+	filenamePdf := fmt.Sprintf("%s.pdf", filename)
+	const writeReadPermission = os.FileMode(0666)
+
+	err = os.WriteFile(filenameLatex, document, writeReadPermission)
+	if err != nil {
+		errorMessage := fmt.Sprintf("Error creating LaTeX file: %s", err)
+		log.Println(errorMessage)
+		http.Error(w, errorMessage, http.StatusInternalServerError)
+		return
+	}
+
+	cmd := exec.Command("pdflatex", "-interaction=nonstopmode", filenameLatex)
+	err = cmd.Run()
+	if err != nil {
+		errorMessage := fmt.Sprintf("Error compiling LaTeX file: %s", err)
+		log.Println(errorMessage)
+		http.Error(w, errorMessage, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/pdf")
+	http.ServeFile(w, r, filenamePdf)
+}
 
 func middleware(handlerFunc http.HandlerFunc) http.HandlerFunc {
-	frontendUrl := fmt.Sprintf("http://%s", FRONTEND_HOST)
+	frontendUrl := fmt.Sprintf("http://%s", frontendHost)
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Origin", frontendUrl)
 		log.Printf("%s %s\n", r.Method, r.RequestURI)
@@ -63,11 +101,12 @@ func middleware(handlerFunc http.HandlerFunc) http.HandlerFunc {
 }
 
 func main() {
-	log.Printf("Server running on %s\n", SERVER_ADDRESS)
+	log.Printf("Server running on %s\n", serverAddress)
 
 	http.HandleFunc("/document", middleware(getDocument))
 	http.HandleFunc("/editDocWebsocket", editDocWebsocketHandler)
+	http.HandleFunc("/compileDocument", compileDocument)
 
-	err := http.ListenAndServe(SERVER_ADDRESS, nil)
+	err := http.ListenAndServe(serverAddress, nil)
 	log.Println(err)
 }
