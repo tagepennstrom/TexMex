b56200d951718c497e15564a44b935225f1c5829
Mauritz Sjödin <mauritz.sjodin3@gmail.com>
2025-05-08 12:24:02 +0200

    fix: ge projektnamn ökande index

--- frontend/src/routes/Footer.svelte
+++ frontend/src/routes/Footer.svelte
@@ -1,20 +1,25 @@
 <script>
-    async function handleClick() {
+    async function startNewProject() {
         const serverUrl = `http://${location.hostname}:8080`;
-        const projectName = 'project-1';
-        const res = await fetch(`${serverUrl}/projects/${projectName}`, {
-            method: "POST",
-        });
-        if (!res.ok) {
-            console.error("Unable to create project:", await res.text());
-            return;
+        let projectNr = 1;
+        while (projectNr < 20) {
+            const projectName = 'project-' + projectNr;
+            const res = await fetch(`${serverUrl}/projects/${projectName}`, {
+                method: "POST",
+            });
+            if (!res.ok) {
+                console.error(await res.text());
+                projectNr++;
+            } else {
+                window.location.href = `/project/${projectName}/EditorArea`;
+                return;
+            }
         }
-        window.location.href = `/project/${projectName}/EditorArea`;
     }
 </script>
 
 <div class="Footer">
-    <button class="button" onclick={handleClick}>Start new project</button>
+    <button class="button" onclick={startNewProject}>Start new project</button>
 </div>
 
 <style>
