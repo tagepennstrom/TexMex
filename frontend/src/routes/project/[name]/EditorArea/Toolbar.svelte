8b66a07a6149eabd844c41f2fdcd26b40e4720e0
Gustav Engvall <gustavengvall@yahoo.se>
2025-05-13 13:14:59 +0200

    style: document re-style

--- frontend/src/routes/project/[name]/EditorArea/Toolbar.svelte
+++ frontend/src/routes/project/[name]/EditorArea/Toolbar.svelte
@@ -39,98 +39,63 @@
 
 <div class="Toolbar">
     
-    <button class="Files" onclick={toggleFilesModal} title ="Show project files">Files</button>
-    <button class="Bold" onclick={insertBold} title ="Bold (crtl + b)"><strong>B</strong></button> 
-    <button class="Italic" onclick={insertItalic} title="Italic (crtl + i)"><em>I</em></button>
-    <button class="Underline" onclick={insertUnderline} title="Underline (crtl + u)"><u>U</u></button>
-    <button class="Newline" onclick={insertNewline} title="New line (crtl + l)">\n</button>
-    <button class="Compile" onclick={compile}>Compile</button>
+    <div class="button-container">
+        <button class="Bold Toolbar-Type" onclick={insertBold} title ="Bold (crtl + b)"><strong>B</strong></button> 
+        <button class="Italic Toolbar-Type" onclick={insertItalic} title="Italic (crtl + i)"><em>I</em></button>
+        <button class="Underline Toolbar-Type" onclick={insertUnderline} title="Underline (crtl + u)"><u>U</u></button>
+        <button class="Newline Toolbar-Type" onclick={insertNewline} title="New line (crtl + l)">\n</button>
+    </div>
+
+    <div class="button-container">
+        <button class="Toolbar-Type other-features" onclick={toggleFilesModal} title ="Show project files">Files</button>
+        <button class="Toolbar-Type other-features" onclick={compile}>Compile</button>
+    </div>
 
 </div>
 
 <style>
 
     .Toolbar{
-    display: flex;
-    gap: 10px;
-    padding: 10px;
-    background-color: #e0f7fa;
-    border-bottom: 1px solid #ccc;
-    align-items: center;
+        display: flex;
+        width: 100%;
+        background-color: rgb(250, 255, 239);
+        border-bottom: 1px solid #ccc;
+        box-shadow: 1px 8px 14px -5px rgba(0,0,0,0.15);
+        justify-content: space-between;
     }
 
-    .Files {
+    .button-container{
+        display: flex;
+        gap: 10px;
+        padding: 10px;
+        align-items: center;
+        margin: 0 1rem;
+
+    }
+
+    .Toolbar-Type {
+        background-color: rgb(123, 197, 53);
         padding: 10px 20px;
-        background-color: rgb(246, 137, 4);
         color: white;
         border: none;
         border-radius: 5px;
         cursor: pointer;
+
     }
 
-    .Bold {
-        padding: 10px 20px;
-        background-color: rgb(246, 137, 4);
-        color: white;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
+    .Toolbar-Type:hover{
+        background-color: rgb(99, 160, 43);
+
     }
 
-    .Bold:hover{
-        background-color: orange;
+    .other-features{
+        background-color: rgb(42, 42, 42);
     }
 
-    .Italic {
-        padding: 10px 20px;
-        background-color: rgb(246, 137, 4);
-        color: white;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-    }
+    .text-function{
 
-    .Italic:hover{
-        background-color: orange;
     }
-
-    .Underline {
-        padding: 10px 20px;
-        background-color: rgb(246, 137, 4);
-        color: white;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-    }
-
-    .Underline:hover{
-        background-color: orange;
-    }
-
-    .Compile {
-        padding: 10px 20px;
-        background-color: rgb(246, 137, 4);
-        color: white;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-    }
-    
-    .Compile:hover{
-        background-color: orange;
-    }
-
-    .Newline {
-        padding: 10px 20px;
-        background-color: rgb(246, 137, 4);
-        color: white;
-        border: none;
-        border-radius: 5px;
-        cursor: pointer;
-    }
-    .Newline:hover{
-        background-color: orange;
-    }
+   
 
 
 </style>
